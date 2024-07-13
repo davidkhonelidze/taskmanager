6c18b143dd854f2fbab316d6f850465993bcec75
davidkhonelidze <davidkhonelidze@gmail.com>
2024-07-13 17:01:36 +0400

    issues page template

---
+++ resources/js/Pages/Issues/List.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Layout from "../../Components/Layout.vue";
+import PageTitle from "../../Components/PageTitle.vue";
+
+const props = defineProps({
+    issues: Object,
+    title: String,
+})
+</script>
+<template>
+    <Layout>
+        <PageTitle :title="props.title"></PageTitle>
+    </Layout>
+</template>
