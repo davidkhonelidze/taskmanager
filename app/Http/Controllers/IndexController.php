7473769cb50370599cf2b64bead0b3979d782cfe
davidkhonelidze <davidkhonelidze@gmail.com>
2024-07-14 00:36:26 +0400

    show issues on home page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\IssuesResource;
+use App\Services\Interfaces\IssueServiceinterface;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 class IndexController extends Controller
 {
+    public function __construct(private IssueServiceinterface $issueService)
+    {
+    }
+
     public function index()
     {
+        $issues = $this->issueService->getIssues(new ParameterBag());
+
         return Inertia::render(
             'Index/Index',
             [
                 'title' => 'Home Page',
+                'issues' => IssuesResource::collection($issues),
             ]
         );
     }
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -2,13 +2,17 @@
 
 import Layout from "../../Components/Layout.vue";
 import PageTitle from "../../Components/PageTitle.vue";
+import Issues from "../../Components/Issues.vue";
 
 const props = defineProps({
     title: String,
+    issues: Object,
 })
 </script>
 <template>
     <Layout>
         <PageTitle :title="props.title"></PageTitle>
+
+        <Issues :issues="props.issues"></Issues>
     </Layout>
 </template>
