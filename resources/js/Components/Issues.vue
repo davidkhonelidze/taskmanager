1ed307542df5847e7578b684cc1d0b116e3e38d5
davidkhonelidze <davidkhonelidze@gmail.com>
2024-07-14 23:39:33 +0400

    delete dialog

--- resources/js/Components/Issues.vue
+++ resources/js/Components/Issues.vue
@@ -4,6 +4,7 @@ import {initFlowbite} from "flowbite";
 
 const props = defineProps({
     issues: Object,
+    deleteIssue: {}
 })
 
 onMounted(() => {
@@ -60,7 +61,10 @@ onMounted(() => {
                     {{issue.subject}}
                 </td>
                 <td class="px-6 py-4">
-                    <button type="button" class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Delete</button>
+                    <button type="button"
+                            @click="deleteIssue(issue.id)"
+                            data-modal-target="popup-modal" data-modal-toggle="popup-modal"
+                            class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Delete</button>
                 </td>
             </tr>
             </tbody>
