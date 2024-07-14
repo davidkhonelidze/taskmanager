6c1f2488f65aebf20c02a85a9c66cee539149051
davidkhonelidze <davidkhonelidze@gmail.com>
2024-07-14 13:24:15 +0400

    issue create button

--- resources/js/Pages/Issues/List.vue
+++ resources/js/Pages/Issues/List.vue
@@ -2,6 +2,7 @@
 import Layout from "../../Components/Layout.vue";
 import PageTitle from "../../Components/PageTitle.vue";
 import Issues from "../../Components/Issues.vue";
+import {Link} from "@inertiajs/vue3";
 
 const props = defineProps({
     issues: Object,
@@ -13,32 +14,18 @@ const props = defineProps({
         <PageTitle :title="props.title"></PageTitle>
 
 
-        <div class="py-2 mb-3">
-
-            <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Status <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-            </svg>
-            </button>
-
-            <!-- Dropdown menu -->
-            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                    </li>
-                </ul>
+        <div class="flex mb-5">
+            <div class="min-w-20">
+                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    <option value="t">filter</option>
+                </select>
             </div>
 
-
+            <div class="min-w-20 ml-auto">
+                <Link href="/issues/create">
+                    <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">New issue</button>
+                </Link>
+            </div>
         </div>
 
         <Issues :issues="props.issues"></Issues>
