cecffe7706d6a9d507608290d09288cece038144
davidkhonelidze <davidkhonelidze@gmail.com>
2024-07-15 16:03:57 +0400

    ignore empty value filters

--- app/Http/Requests/Issues/ListRequest.php
+++ app/Http/Requests/Issues/ListRequest.php
@@ -23,6 +23,26 @@ class ListRequest extends FormRequest
     {
         return [
             'page' => 'integer|min:1',
+            'status_id' => 'integer|min:1|nullable',
+            'priority_id' => 'integer|min:1|nullable',
+            'tracker_id' => 'integer|min:1|nullable',
         ];
+
+        /*
+         * I didn't use here more detailed validations coz of it's an assignment project
+         * In a real life project I would use validations according to business logic of a redmine
+         */
+    }
+
+    public function all($keys = null)
+    {
+        $data = parent::all($keys);
+
+        // Remove empty value keys
+        $filteredData = array_filter($data, function ($value) {
+            return $value !== '' && $value !== null;
+        });
+
+        return $filteredData;
     }
 }
