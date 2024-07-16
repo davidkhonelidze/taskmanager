ead93c45450be817b1c65aeab20a1970c3be6e31
davidkhonelidze <davidkhonelidze@gmail.com>
2024-07-16 07:06:49 +0400

    remove link from footer

--- resources/js/Components/Layout.vue
+++ resources/js/Components/Layout.vue
@@ -19,9 +19,8 @@ const props = defineProps({
     </div>
     <footer class="container mx-auto max-w-[1280px] px-5 relative bottom-0 mt-10 pt-5">
         <div class="flex items-center justify-center gap-2 mb-5 text-gray-500 font-square text-sm">
-            <Link href="#" class="hover:text-moli_blue hover:underline">about</Link>
         </div>
-        <div class="flex items-center justify-center mb-2 text-gray-300 text-sm">
+        <div class="flex items-center justify-center mb-2 text-gray-500 text-sm">
             <span>Tech Assignment</span>
         </div>
     </footer>
